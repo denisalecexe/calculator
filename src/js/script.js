08ac946b507d62c76060983f22fd0975603d5194
denisalecexe <deniszenomiteus@gmail.com>
2026-06-07 22:01:54 +0200

    js: add variables and console.log for testing

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+// VARIABILI
+const resultInput = document.getElementById("result"); // aggancia l'input dove si vedranno i numeri scritti e i risultati
+const cancKey = document.getElementById("canc"); // aggancia il bottone con id "canc" e con l'icona della c all'interno del cerchio
+const backKey = document.getElementById("back-space"); // aggancia il bottone con id "back-space"; con appunto il backspace
+const btnKeys = document.querySelectorAll(".btn"); // aggancia tutti i btn con la classe "btn"
+
+// TEST
+console.log("calculator");
